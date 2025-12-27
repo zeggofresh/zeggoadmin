55268566dfe8b22ebd5ce30c41c85ba93d41c782
zeggofresh <zeggofresh@gmail.com>
2025-12-27 13:13:37 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { HashRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+<HashRouter>
+  <App />
+</HashRouter>
