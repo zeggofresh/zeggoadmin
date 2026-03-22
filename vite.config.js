56640a40698770b336c8f8ec625017bcb0ff7052
zeggofresh <zeggofresh@gmail.com>
2026-03-22 15:21:57 +0530

    dfsg

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/zeggoadmin/",
+  base: "/",
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true
+  }
 });
